86fecbe5e073fb0b0e7cb8dc82f49e47503658c1
Daniel Almasi <almd@pm.me>
2021-02-26 00:32:09 +0000

    Add durchmarsch tests
    Add durchmarsch tests

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,4 +1,78 @@
 // @ts-nocheck
-import { x } from '../index';
+import { Hand } from '../classes/Hand';
+import { DECK, CARD_MAP } from '../globals';
+import {calculateExpectedValue} from '../mini-games/durchmarsch';
 
-test('y', () => {});
+declare global {
+    var MAX_RISK: number;
+}
+globalThis.MAX_RISK = 0.80;
+
+test('durchmarsch ♥ A K O U X IX VIII VII ♦ A K', () => {
+    const hand = new Hand();
+    for (let i = 0; i < 10; i++) {
+        hand.addCard(CARD_MAP.get(i));
+    }
+    const expectedValue = calculateExpectedValue(hand);
+    expect(expectedValue).toBe(6);
+});
+
+test('durchmarsch ♥ K O U X IX VIII VII ♦ A K O', () => {
+    const hand = new Hand();
+    for (let i = 0; i < 11; i++) {
+        if (i == 0) {
+            continue;
+        }
+        hand.addCard(CARD_MAP.get(i));
+    }
+    const expectedValue = calculateExpectedValue(hand);
+    expect(expectedValue).toBe(0);
+});
+
+test('durchmarsch ♥ A O U X IX VIII VII ♦ A K O', () => {
+    const hand = new Hand();
+    for (let i = 0; i < 11; i++) {
+        if (i == 1) {
+            continue;
+        }
+        hand.addCard(CARD_MAP.get(i));
+    }
+    const expectedValue = calculateExpectedValue(hand);
+    expect(expectedValue).toBe(6);
+});
+
+test('durchmarsch ♥ K O U X IX VIII VII ♦ A K ♠ A', () => {
+    const hand = new Hand();
+    for (let i = 0; i < 10; i++) {
+        if (i == 0) {
+            continue;
+        }
+        hand.addCard(CARD_MAP.get(i));
+    }
+    hand.addCard(CARD_MAP.get(16));
+    const expectedValue = calculateExpectedValue(hand);
+    expect(expectedValue).toBe(0);
+});
+
+test('durchmarsch ♥ A K O U X IX VIII VII ♦ A ♠ A', () => {
+    const hand = new Hand();
+    for (let i = 0; i < 9; i++) {
+        hand.addCard(CARD_MAP.get(i));
+    }
+    hand.addCard(CARD_MAP.get(16));
+    const expectedValue = calculateExpectedValue(hand);
+    expect(expectedValue).toBe(6);
+});
+
+test('durchmarsch ♥ A K O U X IX VIII VII ♦ A O', () => {
+    const hand = new Hand();
+    for (let i = 0; i < 11; i++) {
+        if (i == 9) {
+            continue;
+        }
+        hand.addCard(CARD_MAP.get(i));
+    }
+    const expectedValue = calculateExpectedValue(hand);
+    expect(expectedValue).toBe(0);
+    console.log(hand.printWholeHand());
+});
