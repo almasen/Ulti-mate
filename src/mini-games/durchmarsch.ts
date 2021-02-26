9c27819ba4ee9ef5aaf47b22616fba1a88879f40
Daniel Almasi <almd@pm.me>
2021-02-26 00:31:58 +0000

    Implement durchmarsch algorithm
    Implement durchmarsch algorithm

--- src/mini-games/durchmarsch.ts
+++ src/mini-games/durchmarsch.ts
@@ -1,22 +1,96 @@
 import { Card } from '../classes/Card';
+import { Hand } from '../classes/Hand';
 
 const TOTAL_VALUE = 6;
 const GAME_RANK = 7;
 
-const meetsPrerequisites = (hand: Card[]): boolean => {
-    let aceCount = 0;
-    hand.forEach((card: Card) => {
-        if (card.rank.name === 'A') {
-            ++aceCount;
-        }
-    });
-    return aceCount >= 2;
+const meetsPrerequisites = (hand: Hand): boolean => {
+    return hand.aces.length >= 2;
 };
 
-const calculateExpectedValue = (hand: Card[]) => {
+const checkHoles = (suit: Card[]): number => {
+    // console.log(suit);
+    if (suit[0].rank.letter !== "A") {
+        return 0;
+    } else if (suit.length === 1) {
+        return 1;
+    }
+
+    if (suit[1].rank.letter !== "K") {
+        switch (suit.length) {
+            case 7:
+                return 1;
+
+            case 6:
+                return 1/2;
+
+            default:
+                return 0; // other chances are too low
+        }
+    } else if (suit.length === 2) {
+        return 1;
+    }
+
+    if (suit[2].rank.letter !== "O") {
+        switch (suit.length) {
+            case 7:
+            case 6:
+                return 1;
+
+            case 5:
+                return 3/4;
+
+            default:
+                return 0;
+        }
+    } else if (suit.length === 3) {
+        return 1;
+    }
+
+    if (suit[3].rank.letter !== "U") {
+        switch (suit.length) {
+            case 7:
+            case 6:
+            case 5:
+                return 1;
+
+            case 4:
+                return 7/8;
+
+            default:
+                return 0;
+        }
+    } else {
+        return 1;
+    }
+}
+
+const calculateExpectedValue = (hand: Hand):number => {
+    if (hand.length !== 10) {
+        throw new Error(`Invalid hand length ${hand.length}`);
+    }
     if (!meetsPrerequisites(hand)) {
         return 0;
     }
+    let chance = 1;
+    if (hand.hearts.length > 0) {
+        chance *= checkHoles(hand.hearts);
+    }
+    // console.log(chance);
+    // if chance <= MAX_RISK then it can not increase, hence we don't continue
+    if (chance > MAX_RISK && hand.bells.length > 0) {
+        chance *= checkHoles(hand.bells);
+    }
+    // console.log(chance);
+    if (chance > MAX_RISK && hand.leaves.length > 0) {
+        chance *= checkHoles(hand.leaves);
+    }
+    // console.log(chance);
+    if (chance > MAX_RISK && hand.acorns.length > 0) {
+        chance *= checkHoles(hand.acorns);
+    }
+    // console.log(chance);
+    return chance * TOTAL_VALUE;
 };
 
 export { calculateExpectedValue };
