e2d10f06d2f19cfbe25f849c78296fb357cdda59
Daniel Almasi <almd@pm.me>
2021-03-07 18:16:15 +0000

    Refactor CLI options
    Refactor CLI options

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,24 +1,57 @@
+/* tslint:disable:no-console */
 import { Card } from '../classes/Card';
 import { Hand } from '../classes/Hand';
 import { CARD_MAP, DECK } from '../globals';
 import { sortHand } from '../util/hand';
+import { Command, InvalidOptionArgumentError, Option } from 'commander';
+
+const program = new Command();
+
+program
+    .addOption(new Option('-R, --max-risk <percentage>', 'Specify maximum risk in percentages').default(50, '50%'))
+    .option('-H, --custom-hand <ids...>', 'Specify a custom hand via card IDs');
+
+program.parse(process.argv);
+const options = program.opts();
+
+console.log(options);
+
+/* set global MAX_RISK variable */
 
 declare global {
     var MAX_RISK: number;
 }
-const args = process.argv.slice(2);
-globalThis.MAX_RISK = args[0] ? parseInt(args[0], 10) / 100 : 0.5;
+{
+    const parsedValue = parseInt(options.maxRisk, 10);
+    if (isNaN(parsedValue) || parsedValue > 100 || parsedValue < 0) {
+        throw new InvalidOptionArgumentError('Maximum risk must be an integer between 0 and 100.');
+    }
+    globalThis.MAX_RISK = parsedValue / 100;
+}
+
+/* set up user and opponent hands according to params */
 
 let hand: Hand;
 let possibleOpponentCards: Card[];
 
-if (args[1]) {
-    const customHand = args[1].trim().split(',');
+if (options.customHand) {
+    if (!Array.isArray(options.customHand) || options.customHand.length !== 10) {
+        throw new InvalidOptionArgumentError('Custom hand must be a list of 10 integers between 0 and 31 (inclusive).');
+    }
+    const customHand: number[] = [];
+    options.customHand.forEach((e) => {
+        const parsedValue = parseInt(e, 10);
+        if (isNaN(parsedValue) || parsedValue > 31 || parsedValue < 0) {
+            throw new InvalidOptionArgumentError(
+                'Custom hand must be a list of 10 integers between 0 and 31 (inclusive).',
+            );
+        }
+        customHand.push(parsedValue);
+    });
     hand = new Hand();
     possibleOpponentCards = [];
     const cards = new Map(CARD_MAP);
-    customHand.forEach((e) => {
-        const id = parseInt(e, 10);
+    customHand.forEach((id: number) => {
         const card = cards.get(id);
         if (card) {
             hand.addCard(card);
