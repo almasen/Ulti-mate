077b135caaa43c70982f2825336663b6be60d2d8
Daniel Almasi <almd@pm.me>
2021-03-07 16:56:46 +0000

    Add custom hand CLI option
    Add custom hand CLI option

--- src/index.ts
+++ src/index.ts
@@ -6,33 +6,54 @@ import { sortHand } from './util/hand';
 import { calculateChance } from './mini-games/durchmarsch';
 import chalk from 'chalk';
 import { Card } from './classes/Card';
+import { Hand } from './classes/Hand';
 
 declare global {
     var MAX_RISK: number;
 }
 const args = process.argv.slice(2);
 globalThis.MAX_RISK = args[0] ? parseInt(args[0], 10) / 100 : 0.5;
-console.log(`Maximum risk is ${chalk.cyan(MAX_RISK * 100)}%`)
+console.log(`Maximum risk is ${chalk.cyan(MAX_RISK * 100)}%`);
 
-const hand = sortHand(DECK.slice(0, 10));
-// const handMap = new Map(); ??
-const possibleOpponentCards = DECK.slice(10);
+let hand: Hand;
+let possibleOpponentCards: Card[];
+
+if (args[1]) {
+    const customHand = args[1].trim().split(',');
+    hand = new Hand();
+    possibleOpponentCards = [];
+    const cards = new Map(CARD_MAP);
+    customHand.forEach((e) => {
+        const id = parseInt(e, 10);
+        const card = cards.get(id);
+        if (card) {
+            hand.addCard(card);
+        }
+        cards.delete(id);
+    });
+    cards.forEach((value, key) => {
+        possibleOpponentCards.push(value);
+    });
+} else {
+    hand = sortHand(DECK.slice(0, 10));
+    possibleOpponentCards = DECK.slice(10);
+}
 
 const durchmarschChance = calculateChance(hand);
 
 console.log(hand.printWholeHand());
 console.log(`durchmarsch chance is ${chalk.cyan(durchmarschChance * 100)}%`);
 
-console.log("Calculating opponent hands..");
+console.log('Calculating opponent hands..');
 
-console.time("calcOpponentHands");
+console.time('calcOpponentHands');
 const possibleOpponentHands = calculateAndSortPossibleHands(possibleOpponentCards);
-console.timeEnd("calcOpponentHands");
+console.timeEnd('calcOpponentHands');
 
 let opponentDurchmarschChances = 0;
 
 // const possibleHeuristics = new Map();
-console.time("calcOpponentChances");
+console.time('calcOpponentChances');
 possibleOpponentHands.forEach((opponentHand: Card[]) => {
     const sortedOpponentHand = sortHand(opponentHand);
     const oppDurchmarschChance = calculateChance(sortedOpponentHand);
@@ -44,10 +65,12 @@ possibleOpponentHands.forEach((opponentHand: Card[]) => {
     //     possibleHeuristics.set(oppDurchmarschChance, 1);
     // }
 });
-console.timeEnd("calcOpponentChances");
+console.timeEnd('calcOpponentChances');
 
 console.log(opponentDurchmarschChances);
-console.log(`opponent durchmarsch chance is ${chalk.cyan(opponentDurchmarschChances / possibleOpponentHands.length * 100)}%`);
+console.log(
+    `opponent durchmarsch chance is ${chalk.cyan((opponentDurchmarschChances / possibleOpponentHands.length) * 100)}%`,
+);
 
 // // graph bs
 // const keys: number[] = [];
