b1b632cdd87996bbd0cd3b8be3bb2218c8b0f2db
Daniel Almasi <almd@pm.me>
2021-03-15 23:08:41 +0000

    Add betli class
    Add betli class
    
    f
    
    f

---
+++ src/mini-games/classes/betli.ts
@@ -0,0 +1,52 @@
+import { MiniGame } from './mini-game';
+import { Hand } from '../../classes/hand';
+import { Card } from '../../classes/card';
+
+class Betli extends MiniGame {
+    constructor() {
+        super(6, 5, 'betli');
+    }
+
+    meetsPrerequisites(hand: Hand): boolean {
+        return hand.aces.length < 3;
+    }
+
+    countHoles(suit: Card[]): number {
+        if (suit.length === 0) {
+            return 0;
+        }
+        let holes = 0;
+
+        for (let i = suit.length -1; i >= 0; i++) {
+            const element = suit[i];
+            holes += i;
+        }
+        return holes;
+    }
+
+    findWorstCard(hand: Hand): number {
+        return 3;
+    }
+
+    calculateChance(hand: Hand): number {
+        this.validateHand(hand);
+        if (!this.meetsPrerequisites(hand)) {
+            return 0;
+        }
+
+        let holes = 0;
+
+        holes += this.countHoles(hand.hearts);
+        holes += this.countHoles(hand.bells);
+        holes += this.countHoles(hand.leaves);
+        holes += this.countHoles(hand.acorns);
+
+        const chance = 0
+
+        this.logChanceIfApplicable(hand, chance);
+
+        return chance >= MAX_RISK ? chance : 0;
+    }
+}
+
+export { Betli };
