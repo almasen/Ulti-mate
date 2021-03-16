6ae62cf382cc13cdaaec65caf4a7193f3a6dc23d
Daniel Almasi <almd@pm.me>
2021-03-16 21:44:00 +0000

    Implement card removal from hand
    Implement card removal from hand

--- src/classes/Hand.ts
+++ src/classes/Hand.ts
@@ -83,6 +83,76 @@ class Hand extends Array {
         this.push(card);
     }
 
+    removeCard(card: Card) {
+        switch (card.suit.letter) {
+            case 'H':
+                this.removeFromCardArray(this.hearts, card);
+                break;
+
+            case 'B':
+                this.removeFromCardArray(this.bells, card);
+                break;
+
+            case 'L':
+                this.removeFromCardArray(this.leaves, card);
+                break;
+
+            case 'A':
+                this.removeFromCardArray(this.acorns, card);
+                break;
+
+            default:
+                throw new Error('Invalid suit in hand');
+                break;
+        }
+
+        switch (card.rank.letter) {
+            case 'A':
+                this.removeFromCardArray(this.aces, card);
+                break;
+
+            case 'K':
+                this.removeFromCardArray(this.kings, card);
+                break;
+
+            case 'O':
+                this.removeFromCardArray(this.overs, card);
+                break;
+
+            case 'U':
+                this.removeFromCardArray(this.unters, card);
+                break;
+
+            case 'X':
+                this.removeFromCardArray(this.tens, card);
+                break;
+
+            case 'IX':
+                this.removeFromCardArray(this.nines, card);
+                break;
+
+            case 'VIII':
+                this.removeFromCardArray(this.eights, card);
+                break;
+
+            case 'VII':
+                this.removeFromCardArray(this.sevens, card);
+                break;
+
+            default:
+                throw new Error('Invalid rank in hand');
+                break;
+        }
+    }
+
+    private removeFromCardArray(arr: Card[], card: Card) {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i].id === card.id) {
+                arr.splice(i, 1);
+            }
+        }
+    }
+
     printWholeHand(): string {
         let printString = '';
         if (this.hearts.length > 0) {
