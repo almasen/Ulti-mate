6f19e3439ab81592dcdb4131e339d1418d16e625
Daniel Almasi <almd@pm.me>
2021-03-16 20:22:15 +0000

    Fix CI reference error
    Fix CI reference error

--- src/__tests__/Betli-holecount.test.ts
+++ src/__tests__/Betli-holecount.test.ts
@@ -1,7 +1,7 @@
 // @ts-nocheck
 import { Hand } from '../classes/Hand';
 import { DECK, CARD_MAP } from '../globals';
-import { Betli } from '../mini-games/classes/Betli';
+import { Betli } from '../mini-games/classes/Betlixd';
 
 declare global {
     var MAX_RISK: number;
