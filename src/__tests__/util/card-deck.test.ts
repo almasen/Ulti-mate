d66d87f77cf6325825dddd8bd719f7c434d4e5a0
Daniel Almasi <almd@pm.me>
2021-03-23 20:56:45 +0000

    Implement opponent hand calculator tests
    Implement opponent hand calculator tests

---
+++ src/__tests__/util/card-deck.test.ts
@@ -0,0 +1,9 @@
+// @ts-nocheck
+import { calculateAndSortPossibleHands  } from '../../util/card-deck';
+import { DECK } from '../../globals';
+
+test('map freezer blocks changes to map as expected', () => {
+    const opponentCards = DECK.slice(10);
+    const opponentHands = calculateAndSortPossibleHands(opponentCards);
+    expect(opponentHands.length).toBe(646646);
+});
