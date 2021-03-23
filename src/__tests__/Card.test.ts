76257b1c9d47d7e1b4ca4a83766bf6977af95065
Daniel Almasi <almd@pm.me>
2021-03-23 18:03:48 +0000

    Add Card class unit tests
    Add Card class unit tests

---
+++ src/__tests__/Card.test.ts
@@ -0,0 +1,13 @@
+// // @ts-nocheck
+import { Card } from '../classes/Card';
+import { SUITS, RANKS } from '../globals';
+
+test('card to string conversion works as expected', () => {
+    const card: Card = new Card(0, SUITS[0], RANKS[0]);
+    expect(card.toString()).toBe('H-A');
+});
+
+test('card to string conversion works as expected', () => {
+    const card: Card = new Card(0, SUITS[0], RANKS[0]);
+    expect(card.getFullName()).toBe('Ace of Hearts');
+});
